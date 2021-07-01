1af9e489c37cb0946f452ba46cd7c417a5572d6a
Miroslav Csonka <miroslav.csonka@icloud.com>
2021-07-01 10:42:52 +0100

    Exit with non-zero status code on failure

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,7 +11,7 @@ export FORMATTERS=${INPUT_FORMATTERS:-github_status github_pr}
 
 export PRONTO_PULL_REQUEST_ID="$(jq --raw-output .number "${GITHUB_EVENT_PATH}")"
 export PRONTO_GITHUB_ACCESS_TOKEN="${GITHUB_TOKEN}"
-COMMAND="bundle exec pronto run ${GITHUB_WORKSPACE}/${PRONTO_TARGET_PATH} -c ${COMMIT} -r ${RUNNER} -f ${FORMATTERS}"
+COMMAND="bundle exec pronto run ${GITHUB_WORKSPACE}/${PRONTO_TARGET_PATH} -c ${COMMIT} -r ${RUNNER} -f ${FORMATTERS} --exit-code"
 echo "PRONTO_PULL_REQUEST_ID: ${PRONTO_PULL_REQUEST_ID}"
 echo "COMMAND: ${COMMAND}"
 $COMMAND
